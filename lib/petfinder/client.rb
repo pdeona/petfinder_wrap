3436c41a6735b22d0e4a175125c42b2018447547
Pedro De Ona <pdeon003@fiu.edu>
2017-09-24 15:15:33 -0400

    building out client specs

--- lib/petfinder/client.rb
+++ lib/petfinder/client.rb
@@ -1,9 +1,22 @@
 module Petfinder
+  API_BASE_URI = "http://api.petfinder.com/"
+
   class Client
+
+    attr_reader :api_key, :api_secret, :response
+
     def initialize api_key = Petfinder.api_key, api_secret = Petfinder.api_secret
       @api_key = api_key
       @api_secret = api_secret
       raise Petfinder::Error.new "API key is required" unless @api_key
     end
+
+    def get_pet id
+      request_uri = API_BASE_URI << "pet.get?"
+      request_uri << "key=#{@api_key}&id=#{id}&format=json"
+      @response = open(request_uri).read
+      @response
+    end
+
   end
 end
--- spec/petfinder/petfinder-wrap_spec.rb
+++ spec/petfinder/petfinder-wrap_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 
 RSpec.describe Petfinder::Client do
-  context "Client initialize" do
+  context "initialize" do
     let :client_key do
       Petfinder.api_key = "31644babd91732885c1b7962a39b02bd"
     end
@@ -11,12 +11,37 @@ RSpec.describe Petfinder::Client do
       Petfinder.api_secret = "3d72e8837e2db4cb644a60b063905724"
     end
 
-    it "creates a Client object on initialize" do
+    let :c do
       client_key
+      client_secret
       c = Client.new
+    end
+
+    it "creates a Client object on initialize" do
       expect(c).not_to be nil
     end
 
+    it "stores api_key in an instance variable" do
+      expect(c.api_key).to eq Petfinder.api_key
+    end
+
+    it "stores api_secret in an instance variable" do
+      expect(c.api_secret).to eq Petfinder.api_secret
+    end
+
+    context "Client#getPet" do
+
+      it "sends request to the api" do
+        expect{ c.get_pet(12345) }.not_to raise_error
+      end
+
+      it "gets a valid response" do
+        c.get_pet(12345)
+        expect(c.response).not_to be nil
+      end
+
+      it "creates a Pet object"
+    end
 
   end
 
